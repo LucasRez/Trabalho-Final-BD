44512deff99e1dae45f853a4ed959e31e03b6c2c
icevct <icevct@gmail.com>
2016-11-27 19:17:49 -0200

    todas as queries, procedures, view e triggers

--- queries.sql
+++ queries.sql
@@ -1,51 +1,5 @@
-﻿CREATE FUNCTION exemplo(favorecidoNome varchar(80), favorecidoCPF char(14), UnidadeGestCod integer) returns void as $$
-begin 
-
-		insert into Favorecido (FavorecidoCPF, FavorecidoNome, UniGestCod) values (favorecidoCPF, favorecidoNome, UnidadeGestCod);
-	
-end;
-$$
-language 'plpgsql';
-
-CREATE FUNCTION procura(fnome varchar(80)) returns integer as $$
-begin 
-
-	select f.FavorecidoID from Favorecido as f where f.FavorecidoNome = fnome;
-	
-end;
-$$
-language 'plpgsql';
-
-select procura('Victor Vieira');
-
---gasto total dos 6 meses
-select sum(pagamvalor) "Soma Gastos Totais" from pagamento;
-
-select favorecidocpf, pagamvalor from pagamento;
-
---Funcao que recupera todos os gastos por cpf
-CREATE FUNCTION recuperaGastosPorCpf(cpf char(14)) returns table(Preco_dos_gastos real) as $$
-begin 
-	return query select pagamvalor from pagamento where pagamento.favorecidocpf = cpf;
-	
-end;
-$$
-language 'plpgsql';
-
-select recuperaGastosPorCpf('***.648.25*-**');
-
---funcao que recupera todos os gastos por nome
-CREATE FUNCTION recuperaGastosPorNome(nome char(14)) returns table(Preco_dos_gastos real) as $$
-begin 
-	return query select p.pagamvalor from pagamento p join favorecido f on f.favorecidonome = nome and f.favorecidoid = p.favorecidoid;
-	
-end;
-$$
-language 'plpgsql';
-
-select recuperaGastosPorNome('SERGIO RICARDO CALDERINI ROSA');
-
-select * from pagamento join favorecido on pagamento.favorecidocpf = favorecido.favorecidocpf;
+﻿
+--Queries
 
 --query que retorna os gastos de forma decrescente
 select f.favorecidonome, sum(p.pagamvalor) from pagamento p join favorecido f on f.favorecidoid = p.favorecidoid group by f.favorecidonome order by sum(p.pagamvalor) desc;
@@ -63,16 +17,6 @@ select pr.prognome, sum(p.pagamvalor) from pagamento p join acao a on p.acaocod 
 --query que mostra qual acao gastou mais
 select a.acaonome, sum(p.pagamvalor) from pagamento p join acao a on p.acaocod = a.acaocod group by a.acaonome order by sum(p.pagamvalor) desc limit 1;
 
---Funcao que qual programa de determinada acao gastou mais
-CREATE FUNCTION recuperaProgramaMaisCaroAcao(NomeAcao varchar(200)) returns table(NomePrograma varchar(200), valor real) as $$
-begin 
-	return query select pr.prognome, sum(p.pagamvalor) from pagamento p join acao a on a.acaocod = p.acaocod and a.acaonome = NomeAcao join programa pr on pr.progcod = a.progcod group by pr.prognome 
-	order by sum(p.pagamvalor) desc limit 1;
-	
-end;
-$$
-language 'plpgsql';
 
-select recuperaProgramaMaisCaroAcao('Administração da Unidade');
 
 
