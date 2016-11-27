890847d44fe86c9e89c2dc089ba7b3faf9f60cf9
icevct <icevct@gmail.com>
2016-11-27 13:18:36 -0200

    queries ja bem adiantadas e com duas procedures

--- queries.sql
+++ queries.sql
@@ -47,4 +47,19 @@ select recuperaGastosPorNome('SERGIO RICARDO CALDERINI ROSA');
 
 select * from pagamento join favorecido on pagamento.favorecidocpf = favorecido.favorecidocpf;
 
+--query que retorna os gastos de forma decrescente
+select f.favorecidonome, sum(p.pagamvalor) from pagamento p join favorecido f on f.favorecidoid = p.favorecidoid group by f.favorecidonome order by sum(p.pagamvalor) desc;
+--query que retorna os gastos de forma crescente
+select f.favorecidonome, sum(p.pagamvalor) from pagamento p join favorecido f on f.favorecidoid = p.favorecidoid group by f.favorecidonome order by sum(p.pagamvalor) asc;
+--query que retorna o funcionario com o maior gasto
+select f.favorecidonome, sum(p.pagamvalor) from pagamento p join favorecido f on f.favorecidoid = p.favorecidoid group by f.favorecidonome order by sum(p.pagamvalor) desc limit 1;
+--query que retorna o orgao superior que mais gastou
+select osup.orgsupnome, sum(p.pagamvalor) from orgao_superior osup join orgao_subordinado osub on osup.orgsupcod = osub.orgsupcod join unidade_gestora u on u.orgsubcod = osub.orgsubcod
+join favorecido f on f.unigestcod = u.unigestcod join pagamento p on f.favorecidoid = p.favorecidoid group by osup.orgsupnome order by sum(p.pagamvalor) desc limit 1;
+-Qual codigo de pagamento teve o maior gasto?
+select p.pagamcod, sum(p.pagamvalor) from pagamento p group by p.pagamcod order by sum(p.pagamvalor) desc limit 1;
+
+
+
+
 
