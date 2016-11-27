52a37d5f0a6046929565a2a50c9bd822387673b1
icevct <icevct@gmail.com>
2016-11-27 17:40:06 -0200

    falta so o trigger

--- queries.sql
+++ queries.sql
@@ -56,10 +56,23 @@ select f.favorecidonome, sum(p.pagamvalor) from pagamento p join favorecido f on
 --query que retorna o orgao superior que mais gastou
 select osup.orgsupnome, sum(p.pagamvalor) from orgao_superior osup join orgao_subordinado osub on osup.orgsupcod = osub.orgsupcod join unidade_gestora u on u.orgsubcod = osub.orgsubcod
 join favorecido f on f.unigestcod = u.unigestcod join pagamento p on f.favorecidoid = p.favorecidoid group by osup.orgsupnome order by sum(p.pagamvalor) desc limit 1;
--Qual codigo de pagamento teve o maior gasto?
+--Qual codigo de pagamento teve o maior gasto?
 select p.pagamcod, sum(p.pagamvalor) from pagamento p group by p.pagamcod order by sum(p.pagamvalor) desc limit 1;
+--query que mostra qual programa gastou mais
+select pr.prognome, sum(p.pagamvalor) from pagamento p join acao a on p.acaocod = a.acaocod join programa pr on a.progcod = pr.progcod group by pr.prognome order by sum(p.pagamvalor) desc limit 1;
+--query que mostra qual acao gastou mais
+select a.acaonome, sum(p.pagamvalor) from pagamento p join acao a on p.acaocod = a.acaocod group by a.acaonome order by sum(p.pagamvalor) desc limit 1;
 
+--Funcao que qual programa de determinada acao gastou mais
+CREATE FUNCTION recuperaProgramaMaisCaroAcao(NomeAcao varchar(200)) returns table(NomePrograma varchar(200), valor real) as $$
+begin 
+	return query select pr.prognome, sum(p.pagamvalor) from pagamento p join acao a on a.acaocod = p.acaocod and a.acaonome = NomeAcao join programa pr on pr.progcod = a.progcod group by pr.prognome 
+	order by sum(p.pagamvalor) desc limit 1;
+	
+end;
+$$
+language 'plpgsql';
 
-
+select recuperaProgramaMaisCaroAcao('Administração da Unidade');
 
 
