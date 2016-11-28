5169de289bd3879021b13148b7b4b4a3458c2db3
Lucas Rezende <macedolucas@hotmail.com>
2016-11-28 03:25:27 -0200

    Pausa entre os ciclos da UI

--- diarias-e-passagens/src/main/java/br/unb/cic/DiariasEPassagens/ui/Main.java
+++ diarias-e-passagens/src/main/java/br/unb/cic/DiariasEPassagens/ui/Main.java
@@ -1,18 +1,15 @@
 package br.unb.cic.DiariasEPassagens.ui;
 
-import br.unb.cic.DiariasEPassagens.entidades.OrgaoSuperior;
-import br.unb.cic.DiariasEPassagens.negocio.Facade;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Facade facade = new Facade();
-		
-		OrgaoSuperior orgSup;
-		
-		orgSup = facade.recuperaOrgSupPorNome("PRESIDENCIA DA REPUBLICA");
-		
-		System.out.println(orgSup.getNome());
-	}
+		//		UserGUI gui = new UserGUI();
+		//		gui.interfaceUsuario();
+	}	
 
 }
